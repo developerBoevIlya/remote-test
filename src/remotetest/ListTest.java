ddb833ba15f22d41f3a30b73bedb8ce85fca95f4
sfaxi19 <sfaxi19@gmail.com>
2017-07-02 23:06:59 +0300

    init commit

---
+++ src/remotetest/ListTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remotetest;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nikit
+ */
+public class ListTest {
+    private ArrayList<AbstractTest> testList = new ArrayList<AbstractTest>();
+    public void add(AbstractTest test)
+    {
+         this.testList.add(test);
+    }
+    
+}
---
+++ src/remotetest/SimpleTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package remotetest;
+
+/**
+ *
+ * @author nikit
+ */
+//Тест с одним параметром
+public class SimpleTest extends AbstractTest{
+    private String nameSetting;
+
+    @Override
+    public void runTest() {
+     //cчитывание параметров, загрузка в файл и отправка
+    }
+    
+}
